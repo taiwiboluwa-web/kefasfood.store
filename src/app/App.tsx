edeb9dfc97136dc01e6ac0d980ee9a5b2f816cd2
Taiwo Boluwatife <taiwiboluwa@gmail.com>
2026-09-17 12:48:16 +0100

    feat: expose image optimizer in admin

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,12 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
+import { ImageOptimizerButton } from './components/ImageOptimizerButton';
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ImageOptimizerButton />
+    </>
+  );
 }
